1be3d55d5360ce13db436e23cfc404edebf79407
Abhishek Dewan <adewan@umich.edu>
2018-03-16 23:26:13 -0700

    Initial Commit
    Simple iOS project to learn the basics of swift and the autolayout in Xcode.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndex1 = 0
+    var randomDiceIndex2 = 0
+    
+    let diceImageArray = [#imageLiteral(resourceName: "dice1"),#imageLiteral(resourceName: "dice2"),#imageLiteral(resourceName: "dice3"),#imageLiteral(resourceName: "dice4"),#imageLiteral(resourceName: "dice5"),#imageLiteral(resourceName: "dice6")]
+    
+    @IBOutlet weak var mDiceTwo: UIImageView!
+    @IBOutlet weak var mDiceOne: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func rollDice(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        mDiceOne.image = diceImageArray[randomDiceIndex1]
+        mDiceTwo.image = diceImageArray[randomDiceIndex2]
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
 }
 
